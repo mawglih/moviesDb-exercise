c1045830ee431ba0d2a0e9aec27bbb34eff5bac2
Oleg Markoff <mawglih@gmail.com>
2018-09-27 00:48:37 -0400

    search saga

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
-const API_MOVIE = '5bb7a72a3185435136178e1839494fa4'
-const BASE_URL = 'https://api.themoviedb.org/3/discover/movie?'
-export const URL = `${BASE_URL}api_key=${API_MOVIE}`
-
+const API_MOVIE = '5bb7a72a3185435136178e1839494fa4';
+const BASE_URL = 'https://api.themoviedb.org/3/discover/movie?';
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?';
+export const URL = `${BASE_URL}api_key=${API_MOVIE}`;
+export const SEARCH_MOVIE_URL = `${SEARCH_URL}api_key=${API_MOVIE}`;
 export default {};
--- src/sagas/searchMovie.js
+++ src/sagas/searchMovie.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {
-  URL,
+  SEARCH_MOVIE_URL,
 } from 'api';
 import { put, take, call } from 'redux-saga/effects';
 
@@ -10,18 +10,12 @@ import {
   SEARCH_MOVIES_START,
 } from 'actionTypes';
 
-// const searchMovies = (search) => (
-//   axios({
-//     method: 'get',
-//     url: `${URL}&query=${search}`
-//   })
-// )
 
 export function* searchMoviesSaga() {
   try {
     const TERM  = yield take(SEARCH_MOVIES_START);
     yield console.log('TERM: ', TERM.payload);
-    const response = yield axios.get(`${URL}&query=${TERM.payload}`);
+    const response = yield axios.get(`${SEARCH_MOVIE_URL}&query=${TERM.payload}`);
     yield put({
       type: SEARCH_MOVIES_SUCCESS,
       payload: response.data,
